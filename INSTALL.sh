2d7855752d987d2248a2c3497ead0e682e5ded28
firedream89 <briand-kevin@hotmail.fr>
2018-12-30 14:44:06 +0100

    1.01

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,13 +9,13 @@ echo Clone repo Domoserv_pi
 cd $HOME
 git clone https://github.com/firedream89/domoserv_pi.git
 
-if test -d /usr/local/qt5pi; then
+if ! test -d /usr/local/qt5pi; then
 echo copie domoserv_pi/qt5pi to /usr/local
 sudo mkdir /usr/local/qt5pi
 sudo cp -r qt5pi /usr/local/qt5pi
 fi
 
-if test -f /ect/init.d/Domoserv_pi; then
+if ! test -f /ect/init.d/Domoserv_pi; then
 echo copie deamon Domoserv_pi to /etc/init.d
 sudo cp domoserv_pi/Domoserv_pi /etc/init.d/
 fi
