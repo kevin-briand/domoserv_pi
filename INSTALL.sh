94ae7581ec2fb21f55bacba20b9cecf836240948
firedream89 <briand-kevin@hotmail.fr>
2018-12-30 14:48:20 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,13 +6,13 @@ echo Install git
 sudo apt-get install git
 
 echo Clone repo Domoserv_pi
-cd $HOME
+cd -
 git clone https://github.com/firedream89/domoserv_pi.git
 
 if ! test -d /usr/local/qt5pi; then
 echo copie domoserv_pi/qt5pi to /usr/local
 sudo mkdir /usr/local/qt5pi
-sudo cp -r qt5pi /usr/local/qt5pi
+sudo cp -r /domoserv_pi/qt5pi /usr/local/qt5pi
 fi
 
 if ! test -f /ect/init.d/Domoserv_pi; then
